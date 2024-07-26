6742f6fbf9eaf7d12ead9845975fa059d665bf61
Francisco (F4VSE) <kikosgc@users.noreply.github.com>
2024-07-26 18:47:55 +0200

    Adds Portuguese APRS servers

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 """
 fogos-2-aprs
-Version: 1.0
-Date: 2024-07-25
+Version: 1.1
+Date: 2024-07-26
 Description: Configuration file for APRS server credentials and Fogos.pt API settings.
 """
 
 APRS_SERVERS = [
-    {"host": "rotate.aprs2.net", "port": 14580},
-    {"host": "second.server.net", "port": 14580}
+    {"host": "193.137.237.12", "port": 14580},
+    {"host": "ct1ejc.dynip.sapo.pt", "port": 14579},
+    {"host": "euro.aprs2.net", "port": 14580},
+    {"host": "rotate.aprs2.net", "port": 14580}
 ]
 
 APRS_CALLSIGN = "YOUR_CALLSIGN"
