75fbd344beea996aafc179eb3bbe312443df3114
Francisco (F4VSE) <kikosgc@users.noreply.github.com>
2024-07-29 19:34:54 +0200

    Updates config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 """
 fogos-2-aprs
-Version: 1.2
-Date: 2024-07-27
-Description: Configuration file for APRS server credentials.
+Version: 1.3
+Date: 2024-07-29
+Description: Configuration file for APRS server and credentials.
 """
-
+APRS_CALLSIGN = "CALLSIGN"
+APRS_PASSWORD = "PASSWORD"
 APRS_SERVERS = [
     {"host": "193.137.237.12", "port": 14580},
     {"host": "ct1ejc.dynip.sapo.pt", "port": 14579},
     {"host": "euro.aprs2.net", "port": 14580},
     {"host": "rotate.aprs2.net", "port": 14580}
 ]
-
-APRS_CALLSIGN = "YOUR_CALLSIGN"
-APRS_PASSWORD = "YOUR_PASSWORD"
